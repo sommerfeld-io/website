9eb00cd9acb716c1ffcdd2cdeb7d65d424b920f8
sebastian <sebastian@sommerfeld.io>
2023-12-08 21:52:31 +0000

    no more appstack ui bundle (moved into archive project)

--- website/ui/appstack/ui-bundle/src/helpers/uuid.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-module.exports = () => {
-  //return Date.now() // get the number of milliseconds elapsed since 1970-01-01
-  return Math.floor(Math.random() * 100000) // random int
-}
